4306f22b695cf80c4dbea833affbfc1839bbd7a5
Ethan Dean <deanethan3@gmail.com>
2021-02-16 16:14:43 -0500

    add sample simple r script

---
+++ FirstRScript.R
@@ -0,0 +1,12 @@
+# Description of Script Content
+# 16 Feburary 2021
+# Modified on 16 Feburary 2021 to unclude dynamic vectors
+# EMD
+
+rainfall <- runif(10) # total annual rainfall
+biomass <- runif(10) 
+print(rainfall)
+rainfall
+head(rainfall)
+plot(x=rainfall,y=biomass)
+
