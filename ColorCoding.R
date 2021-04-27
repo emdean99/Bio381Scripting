a444340a6d3c4146ff193009812c7e63473aec84
Ethan Dean <deanethan3@gmail.com>
2021-04-27 16:54:17 -0400

    ggplot notes and colors

---
+++ ColorCoding.R
@@ -0,0 +1,186 @@
+# -----------------------------------
+# bar plot geoms and colors in ggplot
+# 27 Apr 2021
+# EMD
+# -----------------------------------
+#
+
+# Preliominaries ------------------------------
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+library(colorblindr)
+library(cowplot)
+library(colorspace)
+library(ggsci)
+library(wesanderson)
+library(TeachingDemos)
+
+char2seed("Dark Star")
+d <- mpg
+
+#################################################
+
+# working with black and white
+
+# gray colors and grey functions
+
+# built in grey colors (0 = black 100 = pure white)
+
+my_greys <- c('grey20','grey50','grey80')
+demoplot(my_greys,'bar')
+
+my_greys2 <- gray(seq(from=0.1,
+                      to=0.9,
+                      length.out=10))
+print(my_greys2)
+demoplot(my_greys2,'heatmap')
+
+# convert color plots to black and white
+p1 <- ggplot(d,aes(x=as.factor(cyl),
+                   y=cty,
+                   fill=as.factor(cyl))) +geom_boxplot()
+plot(p1)
+
+p1_des <- colorblindr::edit_colors(p1, desaturate)
+plot(p1_des)  
+
+# desaturate with custom colors
+
+p2 <- p1 + scale_fill_manual(values=c('red','blue','green','yellow'))
+plot(p2)                             
+
+p2_des <- colorblindr::edit_colors(p2, desaturate)
+plot(p2_des)
+
+# using alpha transparency for histogram
+x1 <- rnorm(n=100,mean=0)
+x2 <- rnorm(n=100,mean=2.7)
+d_frame <- data.frame(v1=c(x1,x2))
+lab <- rep(c('Control','Treatment'),each=100)
+d_frame <- cbind(d_frame,lab)
+str(d_frame)
+
+h1 <- ggplot(d_frame,aes(x=v1,fill=lab))
+h1 + geom_histogram(position = "identity",
+                    alpha=0.5,
+                    color="black")
+# color customization in ggplot
+d <- mpg
+# discrete classification
+# scale_fill_manual() (histogram,boxplots,bars)
+# scale_color_manual() (line,points)
+
+# boxplot with no color
+p_fil <- ggplot(d,aes(x=as.factor(cyl),y=cty))
+p_fil + geom_boxplot()
+
+# boxplot with default fill
+p_fil <- ggplot(d,aes(x=as.factor(cyl),y=cty, 
+                      fill=as.factor(cyl)))
+p_fil + geom_boxplot()
+
+# create custom color palette
+my_cols <- c("red","brown","blue","orange")
+p_fil + geom_boxplot() + 
+  scale_fill_manual(values = my_cols)
+
+# scatterplot with no color
+p_col <- ggplot(d,aes(x=displ,y=cty)) + geom_point(size=3)
+print(p_col)
+
+# scatterplot default ggplot colors
+p_col <- ggplot(d,aes(x=displ,y=cty,col=as.factor(cyl))) + geom_point(size=3)
+print(p_col)
+
+# scatterplot with custom colors 
+p_col + scale_color_manual(values = my_cols)
+
+# consinuous classification for color gradient
+
+# default color
+p_grad <- ggplot(d,aes(x=displ,
+                       y=cty,
+                       col=hwy)) + 
+                   geom_point(size = 3)
+print(p_grad)
+
+# custom sequential gradient (2 color)
+p_grad + scale_color_gradient(low="green",
+                                   high='red')
+
+# custom divergint gradient (3 colors)
+mid <- median(d$hwy)
+p_grad + scale_color_gradient2(midpoint=mid,
+                               low='blue',
+                               mid= 'white',
+                               high='red')
+
+# custom diverging gradient (n colors)
+p_grad + scale_color_gradientn(colors=c('blue',
+                                        'green',
+                                        'yellow',
+                                        'purple',
+                                        'orange'))
+
+# palette tour ------------------------------
+
+library(wesanderson)
+print(wes_palettes)
+demoplot(wes_palettes$BottleRocket1,'pie')
+demoplot(wes_palettes[[2]][1:3],'spine')
+
+my_cols <- wes_palettes$GrandBudapest2[1:4]
+p_fil + geom_boxplot() + scale_fill_manual(values=my_cols)
+
+library(RColorBrewer)
+display.brewer.all()
+display.brewer.all(colorblindFriendly = TRUE)
+
+demoplot(brewer.pal(4,"Accent"),'bar')
+demoplot(brewer.pal(11,"Spectral"),'heatmap')
+my_cols <- c("gray75",brewer.pal(3,"Blues"))         
+print(my_cols)
+
+p_fil + geom_boxplot() + scale_fill_manual(values=my_cols)
+
+# nice tool for seeing hex values
+library(scales)
+show_col(my_cols)
+
+# viridis palettes
+
+### making a heat map 
+x_var <- 1:30 
+y_var <- 1:5
+my_data <- expand.grid(x_var=x_var,y_var=y_var)
+head(my_data)
+z_var <- my_data$x_var + my_data$y_var + 2*rnorm(n=150)
+my_data <- cbind(my_data,z_var)
+
+# default gradient colors in r
+p4 <- ggplot(my_data,aes(x=x_var,
+                         y=y_var,
+                         fill=z_var)) + 
+  geom_tile()
+print(p4)
+
+# user defined divergent palette
+p4 + scale_fill_gradient2(midpoint=19,
+                          low='brown',
+                          mid=gray(0.8),
+                          high='darkblue')
+# use viridis condinuous scale
+p4 + scale_fill_viridis_c()
+
+# option viridis, cividis, magma, inferno, plasma
+
+p4 + scale_fill_viridis_c(option="cividis")
+p4 + scale_fill_viridis_c(option="magma")
+p4 + scale_fill_viridis_c(option="inferno")
+p4 + scale_fill_viridis_c(option="plasma")
+
+#desaturation
+p4 <- p4 + scale_fill_viridis_c()
+p4_des <- edit_colors(p4,desaturate)
+plot(p4_des)
---
+++ ggplot3.R
@@ -0,0 +1,127 @@
+# -----------------------------------
+# bar plot geoms and colors in ggplot
+# 27 Apr 2021
+# EMD
+# -----------------------------------
+#
+
+# Preliominaries ------------------------------
+library(ggplot2)
+library(ggthemes)
+library(pathwork)
+library(colorblindr)
+library(cowplot)
+library(colorspace)
+library(ggsci)
+library(wesanderson)
+library(TeachingDemos)
+
+char2seed("Dark Star")
+d <- mpg
+
+#################################################
+# bar plots
+table(d$drv)
+
+p1 <- ggplot(d,aes(x=drv)) + 
+  geom_bar(color="black",
+           fill="cornsilk")
+print(p1)
+
+# aesthetic mapping for multiple groups in each bar
+
+p1 <- ggplot(d,aes(x=drv,fill=fl)) + 
+  geom_bar()
+print(p1)
+
+# stacking with color transparency, adjust alpha 
+# which is color transparency of each bar
+# alpha = 0 all colors invisible
+# alpha = 1 all colors completely opaque
+
+p1 <- ggplot(d,aes(x=drv,fill=fl)) + 
+  geom_bar(alpha=0.3, position = 'identity')
+print(p1)
+
+# better to use position = fill for stacking
+# with constant height
+p1 <- ggplot(d,aes(x=drv,fill=fl)) + 
+  geom_bar(position="fill")
+print(p1)
+
+# best to use position = dodge generate multiple bars
+p1 <- ggplot(d,aes(x=drv,fill=fl)) + 
+  geom_bar(position="dodge",color="black",
+           size=0.8)
+print(p1)
+
+# more typical bar plot has heights as values (mean, total) 
+d_tiny <- tapply(X=d$hwy,INDEX=as.factor(d$fl),
+                 FUN=mean)
+d_tiny <- data.frame(hwy=d_tiny)
+d_tiny <- cbind(fl=row.names(d_tiny), d_tiny)
+
+p2 <- ggplot(d_tiny,aes(x=fl,y=hwy,fill=fl)) + 
+  geom_col()
+print(p2)
+
+# much better for a box plot!
+p2 <- ggplot(d,aes(x=fl,y=hwy,fill=fl)) + 
+  geom_boxplot()
+print(p2)
+
+
+# overlay the raw data
+p2 <- ggplot(d,aes(x=fl,y=hwy)) + 
+  geom_boxplot(fill="thistle",outlier.shape=NA) + 
+  geom_point()
+print(p2)
+
+# improve visualization of data
+p2 <- ggplot(d,aes(x=fl,y=hwy)) + 
+  geom_boxplot(fill="thistle",outlier.shape=NA) + 
+  geom_point(position=position_jitter(width=0.1,height=0.7),
+             color="gray60",size=2)
+print(p2)
+
+
+# Color ------------------------------
+# hue - wavelength of visible light
+# saturation - intensity, vibrance 
+# lightness - black to white
+# red, blue, green
+# named colors in R
+
+# Aesthetics
+# attractive colors
+# - large geoms (bars, boxplots), light, pale colors
+# - small geoms (points, lines), dark, vibrant colors
+# color palettes that are visible to color blind
+# color palettes that convert well to black and white
+
+# Information content
+# use colors to group similar treatments
+# neutral colors (black,grey, white) for control groups
+# symbolic colors (heat=red,cool=blue,photosynthesis/growth=green,oligotrophic=blue,infected=red)
+# dyes or stains, or even colors of organisms
+
+# discrete scales - distinct groups
+
+# continuous scales (as in a heat map)
+# monochromatic (different shades of one color)
+# 2-tone chromatic scale (from color x to color y)
+# 3-tone chromatic scale (from color x through color y to color z)
+
+# consistent color scheme for manuscript
+# Use consistent colors within and between your figures
+
+my_cols <- c('#ca0020', '#f4a582','#92c5de','#0571b0')
+demoplot(my_cols,"map")
+demoplot(my_cols,"bar")
+demoplot(my_cols,"scatter")
+demoplot(my_cols,"spine")
+demoplot(my_cols,"heatmap")
+demoplot(my_cols,"perspective")
+
+my_r_colors <- c("red","brown","cyan","green")
+demoplot(my_r_colors,"pie")
